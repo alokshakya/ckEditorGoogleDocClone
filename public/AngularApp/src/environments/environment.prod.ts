971dde351a241afe7857f29f73ae02a66a804f5c
alokshakya <alokshakyacode@gmail.com>
2021-05-08 12:59:06 +0530

    removed actual values from environment

--- public/AngularApp/src/environments/environment.prod.ts
+++ public/AngularApp/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 
 export const environment = {
    production: true,
-   UPLOAD_URL: "https://80315.cke-cs.com/easyimage/upload",
-   WEB_SOCKET_URL: "wss://80315.cke-cs.com/ws",
-   TOKEN_URL: "http://localhost:3000/users/"
 };
